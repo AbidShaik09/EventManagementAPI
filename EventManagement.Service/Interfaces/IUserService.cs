900d4c5fa4285ea4e81f064947ac6dd77e7d603b
Venkat Sai <venkatsai_jeevanagari@epam.com>
2023-08-12 10:45:50 +0530

    Add service

---
+++ EventManagement.Service/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using EventManagement.Data.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagement.Service.Interfaces
+{
+    public interface IUserService
+    {
+        bool SignUp(UserSignUpDTO request);
+    }
+}
