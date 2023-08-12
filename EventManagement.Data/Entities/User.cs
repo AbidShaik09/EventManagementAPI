a1b0aa0886934db1393bf98c4ee6ee0b322909fe
Venkat Sai <venkatsai_jeevanagari@epam.com>
2023-08-12 10:33:05 +0530

    first commit

---
+++ EventManagement.Data/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagement.Data.Entities
+{
+    [Table("Users")]
+    public class User : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string IdCardNo { get; set; }
+        public string Mobile { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ EventManagementAPI/Controllers/SignUpController.cs
@@ -0,0 +1,20 @@
+﻿using EventManagement.Data.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Http;
+
+namespace EventManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SignUpController : ControllerBase
+    {
+        [Route("")]
+        [HttpPost]
+        public HttpResponseMessage Register([FromBody] UserSignUpDTO user)
+        {
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+    }
+}
